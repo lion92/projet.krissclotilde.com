c051fe09614f02fcabdd36e2d3f45075dd6362b7
kriss-clotilde <kriss.clotilde@gmail.com>
2022-06-15 17:52:27 +0200

    modif css by scrolling

---
+++ js/scroll.js
@@ -0,0 +1,16 @@
+/*onst nav=document.querySelector(".container_principal");
+
+window.addEventListener('scroll',()=>{
+    console.log(window.scrollY);
+    if(window.scrollY>300 && window.scrollY<600){
+        console.log("dd");
+       nav.classList.add("white");
+      //
+    }
+
+    else{
+        nav.classList.remove("white");
+       // navpage.classList.remove("white");
+
+    }
+})*/
